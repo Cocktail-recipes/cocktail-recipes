2193c34c40e1520b299d554684973a9fcf2366b2
Wilfried Wurch <wil@wurch.de>
2022-10-13 11:47:51 +0200

    add keep alive to index.routes.js

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
+const Recipe = require('../models/recipe.model')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+/* GET keep alive */
+router.get('/keep-alive', (req, res, next) => {
+  Recipe.find()
+    .then(() => {
+      res.status(200).json({ message: 'It worked' });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "It didn't work" });
+    });
 });
 
 module.exports = router;
